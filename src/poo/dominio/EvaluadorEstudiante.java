83105378f03afda6b08139fb6490d697d3e0e8ff
Gerardo-Rioss <gerarios26@gmail.com>
2025-10-18 23:10:11 -0300

    Agregado calcularVariacion

--- src/poo/dominio/EvaluadorEstudiante.java
+++ src/poo/dominio/EvaluadorEstudiante.java
@@ -1,5 +1,5 @@
 package poo.dominio;
-
+//Verificar si aprobó todas, ninguna o algunas
 public class EvaluadorEstudiante {
     public static String evaluarAprobacion(Estudiante estudiante){
         int [] notas = estudiante.getNotas();
@@ -18,4 +18,23 @@ public class EvaluadorEstudiante {
 
         }
     }
+
+    //Prueba mas inconsistente
+    public static String calcularVariacion(Estudiante estudiante){
+        int [] notas = estudiante.getNotas();
+        int maxVariacion= 0;
+        int indice = 0;
+        for (int i = 0; i < notas.length-1; i++) {
+            int variacion = Math.abs(notas[i]- notas[i+1]);
+            if (variacion> maxVariacion){
+                maxVariacion= variacion;
+                indice= i;
+            }
+        }
+        return "El Mayor salto fué de "+ maxVariacion+" puntos entre la prueba "+ (indice+1)+" y la prueba "+(indice+2)+".";
+
+    }
 }
+
+
+
